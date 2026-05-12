b0c7d9f1f5f743fb44db87798725869ffd0249b5
Ilja Miseljov <wovanmilja@gmail.com>
2026-05-12 11:41:49 +0300

    42

--- server.mjs
+++ server.mjs
@@ -3,6 +3,6 @@ const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
 });
-server.listen(80, '127.0.0.1', () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
